227a6af169598d7a8dde3d0e7f1804bd64f31bd0
leviv <villarreallevi@gmail.com>
2018-02-26 15:51:48 -0600

    Content update

--- articles/partisan-bickering-puts-state-welfare-program-in-peril/data.php
+++ articles/partisan-bickering-puts-state-welfare-program-in-peril/data.php
@@ -5,13 +5,19 @@ $author = 'Jim Phipps';
 $pubdate = 'Feb. 26, 2017';
 $featured_img = '../../dist/img/tideAd.png';
 
+if ($race.substr(0,1) == 'a') {
+    $race1 = 'a ' . $race;
+} else {
+    $race1 = $race;
+}
+
 $featured_img_caption = '';
 $article = '
 <p><strong>LANSING</strong> – Republicans and Democrats remain deadlocked in the debate over the future of the state welfare program, and the state will not have the money to continue the current program if an agreement has not been reached by March 1st.</p>
 
-<p>“Because politicians can’t come to an agreement, my son and I may not have food next month,” says Wendy Brown. Brown is a' . $race . ' woman in her early 30s with a high school degree and has been receiving aid from the Family Independence Program (FIP) for the past year to help support herself and her three-year-old son.</p>
+<p>“Because politicians can’t come to an agreement, my son and I may not have food next month,” says Wendy Brown. Brown is a' . $race1 . ' woman in her early 30s with a high school degree and has been receiving aid from the Family Independence Program (FIP) for the past year to help support herself and her three-year-old son.</p>
 
-<p>Brown went on to say that she has been following the ongoing debates closely. “This is something that is going to affect me and my community,” Brown says, “A lot of people I know will be paying attention.” Brown, who lives in a predominantly African-American neighborhood outside of Grand Rapids, believes that her friends and neighbors have a lot to lose if the parties cannot reach an agreement.</p>
+<p>Brown went on to say that she has been following the ongoing debates closely. “This is something that is going to affect me and my community,” Brown says, “A lot of people I know will be paying attention.” Brown, who lives in a predominantly ' . $race . ' neighborhood outside of Grand Rapids, believes that her friends and neighbors have a lot to lose if the parties cannot reach an agreement.</p>
 
 <p>FIP is the cash assistance provided to families living below the poverty line. Along with food stamps and Medicaid, FIP comprises the central tier of the welfare program for the state of Michigan, and it is where Democrats and Republicans disagree most vehemently. While the legislature does not seem close to passing a bill, it appears as though every part of the program is up for negotiation. Some of the proposed changes to FIP would: </p>
 
